5ec252155a8035c6eef6dcb4df4e65fb22d79d0a
songseongbin <ssbin0916@naver.com>
2024-03-17 21:51:10 +0900

    Guitar Shop 프로젝트 생성

---
+++ src/main/java/com/project/guitarShop/GuitarShopApplication.java
@@ -0,0 +1,13 @@
+package com.project.guitarShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarShopApplication.class, args);
+	}
+
+}
