974aa463f1b9316545d4e7eae443407b3b33730f
songseongbin <ssbin0916@naver.com>
2024-03-17 21:57:38 +0900

    테스트

---
+++ src/test/java/com/project/guitarShop/GuitarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.guitarShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
