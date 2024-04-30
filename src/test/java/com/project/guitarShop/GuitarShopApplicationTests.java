f846a4b0178ec67cf106c1b9b6677d0c14afd4b7
songseongbin <ssbin0916@naver.com>
2024-04-30 21:42:34 +0900

    QueryDsl 동작 확인 테스트

--- src/test/java/com/project/guitarShop/GuitarShopApplicationTests.java
+++ src/test/java/com/project/guitarShop/GuitarShopApplicationTests.java
@@ -1,13 +1,34 @@
 package com.project.guitarShop;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class GuitarShopApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
 	}
 
 }
