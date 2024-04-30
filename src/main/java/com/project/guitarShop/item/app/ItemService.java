1f491149df2963cba29219906d8d10ee00db5974
songseongbin <ssbin0916@naver.com>
2024-05-01 00:57:42 +0900

    브랜드 별 가격 정렬 서비스

--- src/main/java/com/project/guitarShop/item/app/ItemService.java
+++ src/main/java/com/project/guitarShop/item/app/ItemService.java
@@ -1,5 +1,7 @@
 package com.project.guitarShop.item.app;
 
+import com.project.guitarShop.item.domain.Brand;
+
 import java.util.List;
 
 public interface ItemService {
@@ -7,4 +9,6 @@ public interface ItemService {
     ItemResponse save(ItemRequest itemRequest);
 
     List<ItemResponse> findALlBySortPrice(boolean ascending);
+
+    List<ItemResponse> findAllBySortBrandAndPrice(boolean ascending, Brand brand);
 }
--- src/main/java/com/project/guitarShop/item/app/ItemServiceImpl.java
+++ src/main/java/com/project/guitarShop/item/app/ItemServiceImpl.java
@@ -1,8 +1,10 @@
 package com.project.guitarShop.item.app;
 
+import com.project.guitarShop.item.domain.Brand;
 import com.project.guitarShop.item.domain.Item;
 import com.project.guitarShop.item.domain.QItem;
 import com.project.guitarShop.item.repository.ItemRepository;
+import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -36,9 +38,23 @@ public class ItemServiceImpl implements ItemService {
     public List<ItemResponse> findALlBySortPrice(boolean ascending) {
         OrderSpecifier<Integer> orderSpecifier = ascending ? qItem.price.asc() : qItem.price.desc();
         return queryFactory
-                .select(new QItemResponse(qItem.name, qItem.price, qItem.quantity, qItem.category, qItem.brand))
+                .select(new QItemResponse(
+                        qItem.name, qItem.price, qItem.quantity, qItem.category, qItem.brand))
                 .from(qItem)
                 .orderBy(orderSpecifier)
+                .fetch();
+    }
+
+    @Override
+    public List<ItemResponse> findAllBySortBrandAndPrice(boolean ascending, Brand brand) {
+        Order order = ascending ? Order.ASC : Order.DESC;
+
+        return queryFactory
+                .select(new QItemResponse(
+                        qItem.name, qItem.price, qItem.quantity, qItem.category, qItem.brand))
+                .from(qItem)
+                .where(qItem.brand.eq(brand))
+                .orderBy(qItem.brand.asc(), new OrderSpecifier<>(order, qItem.price))
                 .fetch();
     }
 }
