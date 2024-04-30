65522a56d291d924adfe49b986ac583711bbc517
songseongbin <ssbin0916@naver.com>
2024-05-01 00:57:52 +0900

    브랜드 별 가격 정렬 서비스 테스트

--- src/test/java/com/project/guitarShop/item/app/ItemServiceImplTest.java
+++ src/test/java/com/project/guitarShop/item/app/ItemServiceImplTest.java
@@ -38,6 +38,7 @@ class ItemServiceImplTest {
 
     @Test
     void findAllBySortPriceAscending() {
+        //given
         ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
         ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
         ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
@@ -49,10 +50,10 @@ class ItemServiceImplTest {
         itemService.save(itemRequest4);
         itemService.save(itemRequest5);
 
-        //given
+        //when
         List<ItemResponse> items = itemService.findALlBySortPrice(true);
 
-        //when //then
+        //then
         assertThat(items).isNotEmpty();
 
         int prevPrice = Integer.MIN_VALUE;
@@ -64,6 +65,7 @@ class ItemServiceImplTest {
 
     @Test
     void findAllBySortPriceDescending() {
+        //given
         ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
         ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
         ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
@@ -75,10 +77,10 @@ class ItemServiceImplTest {
         itemService.save(itemRequest4);
         itemService.save(itemRequest5);
 
-        //given
+        //when
         List<ItemResponse> items = itemService.findALlBySortPrice(false);
 
-        //when //then
+        //then
         assertThat(items).isNotEmpty();
 
         int prevPrice = Integer.MAX_VALUE;
@@ -88,4 +90,51 @@ class ItemServiceImplTest {
         }
     }
 
+    @Test
+    void findAllBySortBrandAndPriceAscending() {
+        //given
+        ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
+        ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
+        ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
+        ItemRequest itemRequest4 = new ItemRequest("name", 40000, 10, Category.ELECTRIC_GUITAR, Brand.SUHR);
+        ItemRequest itemRequest5 = new ItemRequest("name", 50000, 10, Category.ELECTRIC_GUITAR, Brand.PRS);
+        itemService.save(itemRequest1);
+        itemService.save(itemRequest2);
+        itemService.save(itemRequest3);
+        itemService.save(itemRequest4);
+        itemService.save(itemRequest5);
+
+        //when
+        List<ItemResponse> items = itemService.findAllBySortBrandAndPrice(true, Brand.JAMESTYLER);
+
+        //then
+        assertEquals(3, items.size());
+        for (int i = 0; i < items.size() - 1; i++) {
+            assertTrue(items.get(i).getPrice() <= items.get(i + 1).getPrice());
+        }
+    }
+
+    @Test
+    void findAllBySortBrandAndPriceDescending() {
+        //given
+        ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
+        ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
+        ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
+        ItemRequest itemRequest4 = new ItemRequest("name", 40000, 10, Category.ELECTRIC_GUITAR, Brand.SUHR);
+        ItemRequest itemRequest5 = new ItemRequest("name", 50000, 10, Category.ELECTRIC_GUITAR, Brand.PRS);
+        itemService.save(itemRequest1);
+        itemService.save(itemRequest2);
+        itemService.save(itemRequest3);
+        itemService.save(itemRequest4);
+        itemService.save(itemRequest5);
+
+        //when
+        List<ItemResponse> items = itemService.findAllBySortBrandAndPrice(false, Brand.JAMESTYLER);
+
+        //then
+        assertEquals(3, items.size());
+        for (int i = 0; i < items.size() - 1; i++) {
+            assertTrue(items.get(i).getPrice() >= items.get(i + 1).getPrice());
+        }
+    }
 }
