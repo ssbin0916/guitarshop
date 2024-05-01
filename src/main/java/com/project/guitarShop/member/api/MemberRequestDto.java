05c4db91c54fce1ce5fa7e81eaf6080b6059ca1b
songseongbin <ssbin0916@naver.com>
2024-05-01 12:07:30 +0900

    이름 변경

--- src/main/java/com/project/guitarShop/member/api/MemberRequestDto.java
+++ src/main/java/com/project/guitarShop/member/api/MemberRequestDto.java
@@ -6,11 +6,11 @@ import lombok.Builder;
 
 import java.util.List;
 
-public record MemberRequestDTO(String loginId, String password, String confirmPassword, String name, int age, String phone,
+public record MemberRequestDto(String loginId, String password, String confirmPassword, String name, int age, String phone,
                                String email, String role, Address address, List<Order> orders) {
 
     @Builder
-    public MemberRequestDTO {
+    public MemberRequestDto {
 
     }
 
--- src/main/java/com/project/guitarShop/member/api/MemberResponseDto.java
+++ src/main/java/com/project/guitarShop/member/api/MemberResponseDto.java
@@ -3,10 +3,10 @@ package com.project.guitarShop.member.api;
 import com.project.guitarShop.address.domain.Address;
 import lombok.Builder;
 
-public record MemberResponseDTO(String name, int age, String phone, String email, Address address) {
+public record MemberResponseDto(String name, int age, String phone, String email, Address address) {
 
     @Builder
-    public MemberResponseDTO{
+    public MemberResponseDto {
 
     }
 }
