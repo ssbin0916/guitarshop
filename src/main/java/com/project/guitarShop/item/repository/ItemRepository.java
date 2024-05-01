174c250c8761d6f9fe75607a62aee0123dab09d8
songseongbin <ssbin0916@naver.com>
2024-05-01 11:58:51 +0900

    이름으로 찾기 추가

--- src/main/java/com/project/guitarShop/item/repository/ItemRepository.java
+++ src/main/java/com/project/guitarShop/item/repository/ItemRepository.java
@@ -1,8 +1,13 @@
 package com.project.guitarShop.item.repository;
 
+import com.project.guitarShop.item.app.ItemResponse;
 import com.project.guitarShop.item.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Optional<ItemResponse> findByName(String name);
 
 }
