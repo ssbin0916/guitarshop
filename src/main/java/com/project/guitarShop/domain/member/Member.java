3dd9106471f512b6f38564bd924a9693697da406
songseongbin <ssbin0916@naver.com>
2024-05-03 11:01:08 +0900

    엔티티 생성자 수정

--- src/main/java/com/project/guitarShop/domain/member/Member.java
+++ src/main/java/com/project/guitarShop/domain/member/Member.java
@@ -3,9 +3,10 @@ package com.project.guitarShop.domain.member;
 import com.project.guitarShop.domain.address.Address;
 import com.project.guitarShop.domain.order.Order;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +37,6 @@ public class Member {
     private String phone;
     @Column(nullable = false)
     private String email;
-    @Column(nullable = false)
     private String role;
 
     @Embedded
@@ -46,15 +46,8 @@ public class Member {
     private List<Order> orders = new ArrayList<>();
 
     @Builder
-    public Member(String loginId, String password, String confirmPassword, String name, int age, String phone, String email, String role, Address address, List<Order> orders) {
-        Assert.notNull(loginId, "아이디 입력은 필수입니다.");
-        Assert.notNull(password, "비밀번호 입력은 필수입니다.");
-        Assert.notNull(confirmPassword, "비밀번호 확인 입력은 필수입니다.");
-        Assert.notNull(name, "이름 입력은 필수입니다.");
-        Assert.notNull(age, "나이 입력은 필수입니다.");
-        Assert.notNull(phone, "전화번호 입력은 필수입니다.");
-        Assert.notNull(email, "이메일 입력은 필수입니다.");
-        Assert.notNull(address, "주소 입력은 필수입니다.");
+    public Member(Long id, String loginId, String password, String confirmPassword, String name, int age, String phone, String email, String role, Address address, List<Order> orders) {
+        this.id = id;
         this.loginId = loginId;
         this.password = password;
         this.confirmPassword = confirmPassword;
@@ -66,19 +59,4 @@ public class Member {
         this.address = address;
         this.orders = orders;
     }
-
-//    public static Member toDomain(MemberRequest memberRequest) {
-//        return Member.builder()
-//                .loginId(memberRequest.getLoginId())
-//                .password(memberRequest.getPassword())
-//                .confirmPassword(memberRequest.getConfirmPassword())
-//                .name(memberRequest.getName())
-//                .age(memberRequest.getAge())
-//                .phone(memberRequest.getPhone())
-//                .email(memberRequest.getEmail())
-//                .role(memberRequest.getRole())
-//                .address(memberRequest.getAddress())
-//                .orders(memberRequest.getOrders())
-//                .build();
-//    }
 }
