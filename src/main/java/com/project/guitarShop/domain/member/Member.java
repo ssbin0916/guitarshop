8933c60c2f485c481e0a0eca3bf259565b3c2d5c
songseongbin <ssbin0916@naver.com>
2024-05-04 19:00:50 +0900

    변수 추가, 업데이트 입력 메서드 작성

--- src/main/java/com/project/guitarShop/domain/member/Member.java
+++ src/main/java/com/project/guitarShop/domain/member/Member.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +33,8 @@ public class Member {
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
-    private int age;
+    private String rrn;
+    private String gender;
     @Column(nullable = false)
     private String phone;
     @Column(nullable = false)
@@ -46,17 +48,39 @@ public class Member {
     private List<Order> orders = new ArrayList<>();
 
     @Builder
-    public Member(Long id, String loginId, String password, String confirmPassword, String name, int age, String phone, String email, String role, Address address, List<Order> orders) {
+    public Member(Long id, String loginId, String password, String confirmPassword, String name, String rrn, String gender, String phone, String email, String role, Address address, List<Order> orders) {
         this.id = id;
         this.loginId = loginId;
         this.password = password;
         this.confirmPassword = confirmPassword;
         this.name = name;
-        this.age = age;
+        this.rrn = rrn;
+        this.gender = gender;
         this.phone = phone;
         this.email = email;
         this.role = role;
         this.address = address;
         this.orders = orders;
+    }
+
+    public void updateInfo(String updatePhone, String updateEmail, Address updateAddress) {
+        if (updatePhone != null && !this.phone.equals(updatePhone)) {
+            this.phone = updatePhone;
+        }
+        if (updateEmail != null && !this.email.equals(updateEmail)) {
+            this.email = updateEmail;
+        }
+        if (updateAddress != null && !this.address.equals(updateAddress)) {
+            this.address = updateAddress;
+        }
+    }
+
+    public void updatePassword(String updatePassword, String updateConfirmPassword, BCryptPasswordEncoder passwordEncoder) {
+        if (updatePassword != null && !passwordEncoder.matches(updatePassword, this.password)) {
+            this.password = passwordEncoder.encode(updatePassword);
+        }
+        if (updateConfirmPassword != null && !passwordEncoder.matches(updateConfirmPassword, this.confirmPassword)) {
+            this.confirmPassword = passwordEncoder.encode(updateConfirmPassword);
+        }
     }
 }
