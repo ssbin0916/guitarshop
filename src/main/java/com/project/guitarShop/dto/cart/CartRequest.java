d5eb10c48b9cfe6858efc2e3b1ad211f5f71cee4
songseongbin <ssbin0916@naver.com>
2024-05-04 16:58:22 +0900

    entity, dt 연결

--- src/main/java/com/project/guitarShop/dto/cart/CartRequest.java
+++ src/main/java/com/project/guitarShop/dto/cart/CartRequest.java
@@ -1,5 +1,6 @@
 package com.project.guitarShop.dto.cart;
 
+import com.project.guitarShop.domain.cart.Cart;
 import com.project.guitarShop.domain.member.Member;
 import com.project.guitarShop.domain.orderItem.OrderItem;
 import lombok.Getter;
@@ -14,12 +15,18 @@ public class CartRequest {
     public static class AddCartRequestDto {
         private Member member;
         private List<OrderItem> orderItems;
+
+        public Cart toDomain() {
+            return Cart.builder()
+                    .member(member)
+                    .orderItems(orderItems)
+                    .build();
+        }
     }
 
     @Getter
     @Setter
     public static class CartListRequestDto {
-
 
     }
 }
