e3816814250521798baa525133f2131ab5126405
songseongbin <ssbin0916@naver.com>
2024-05-07 15:23:09 +0900

    아이템 저장, 검색, 정렬 테스트 완료

--- src/test/java/com/project/guitarShop/GuitarShopApplicationTests.java
+++ src/test/java/com/project/guitarShop/GuitarShopApplicationTests.java
@@ -1,48 +1,13 @@
 package com.project.guitarShop;
 
-import com.project.guitarShop.domain.address.Address;
-import com.project.guitarShop.domain.member.Member;
-import com.project.guitarShop.member.domain.QMember;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
-@Transactional
 class GuitarShopApplicationTests {
 
-	@Autowired
-	EntityManager em;
+    @Test
+    void contextLoads() {
 
-	@Test
-	void contextLoads() {
-
-		Member member = new Member(
-				"loginId",
-				"password",
-				"password",
-				"name",
-				29,
-				"phone",
-				"email",
-				"ROLE_USER",
-				new Address("addr1", "addr2", "addr3"),
-				null);
-
-		em.persist(member);
-
-		JPAQueryFactory query = new JPAQueryFactory(em);
-		QMember qMember = QMember.member;
-
-		Member result = query
-				.selectFrom(qMember)
-				.fetchOne();
-
-		Assertions.assertThat(result).isEqualTo(member);
-	}
-
+    }
 }
--- src/test/java/com/project/guitarShop/service/item/ItemServiceTest.java
+++ src/test/java/com/project/guitarShop/service/item/ItemServiceTest.java
@@ -2,9 +2,10 @@ package com.project.guitarShop.service.item;
 
 import com.project.guitarShop.domain.item.Brand;
 import com.project.guitarShop.domain.item.Category;
-import com.project.guitarShop.dto.item.ItemRequest;
-import com.project.guitarShop.dto.item.ItemResponse;
-import com.project.guitarShop.service.item.ItemService;
+import com.project.guitarShop.dto.item.ItemRequest.AddItemRequest;
+import com.project.guitarShop.dto.item.ItemRequest.FindItemRequest;
+import com.project.guitarShop.dto.item.ItemResponse.AddItemResponse;
+import com.project.guitarShop.dto.item.ItemResponse.FindItemResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,11 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static com.project.guitarShop.domain.item.Brand.*;
+import static com.project.guitarShop.domain.item.Category.ELECTRIC_GUITAR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
+//@Rollback(value = false)
 class ItemServiceTest {
 
     @Autowired
@@ -25,118 +28,141 @@ class ItemServiceTest {
     @Test
     void save() {
         //given
-        ItemRequest itemRequest = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
+        AddItemRequest request = AddItemRequest.builder()
+                .name("제임스 타일러")
+                .price(7500000)
+                .quantity(1)
+                .category(ELECTRIC_GUITAR)
+                .brand(JAMESTYLER)
+                .build();
 
         //when
-        ItemResponse saveItem = itemService.save(itemRequest);
+        AddItemResponse response = itemService.save(request);
 
         //then
-        assertEquals("name", saveItem.getName());
-        assertEquals(10000, saveItem.getPrice());
-        assertEquals(10, saveItem.getQuantity());
-        assertEquals(Category.ELECTRIC_GUITAR, saveItem.getCategory());
-        assertEquals(Brand.JAMESTYLER, saveItem.getBrand());
+        assertEquals("제임스 타일러", response.getName());
+        assertEquals(7500000, response.getPrice());
+        assertEquals(1, response.getQuantity());
+        assertEquals(ELECTRIC_GUITAR, response.getCategory());
+        assertEquals(JAMESTYLER, response.getBrand());
     }
+
+    @Test
+    void search() {
+        //given
+        init();
+
+        //when
+        List<FindItemResponse> items = itemService.search(new FindItemRequest(null, null, null, null, true));
+
+        //then
+        assertEquals(5, items.size());
+        assertEquals("PRS", items.get(0).getName());
+        assertEquals("제임스 타일러", items.get(1).getName());
+        assertEquals(PRS, items.get(0).getBrand());
+
+
+    }
+
+    @Test
+    void searchByName() {
+        //given
+        init();
+
+        //when
+        List<FindItemResponse> items = itemService.search(new FindItemRequest("타일러", null, null, null, true));
+
+        //then
+        assertEquals(2, items.size());
+    }
+
+    @Test
+    void brandInCategory() {
+        //given
+        init();
+
+        //when
+        List<FindItemResponse> items = itemService.search(new FindItemRequest(null, null, ELECTRIC_GUITAR, JAMESTYLER, true));
+
+        //then
+        assertEquals(2, items.size());
+    }
+
 
     @Test
     void findAllBySortPriceAscending() {
         //given
-        ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest4 = new ItemRequest("name", 40000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest5 = new ItemRequest("name", 50000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        itemService.save(itemRequest1);
-        itemService.save(itemRequest2);
-        itemService.save(itemRequest3);
-        itemService.save(itemRequest4);
-        itemService.save(itemRequest5);
+        init();
 
         //when
-        List<ItemResponse> items = itemService.findALlBySortPrice(true);
+        List<FindItemResponse> items = itemService.search(new FindItemRequest(null, null, null, null, true));
+
 
         //then
-        assertThat(items).isNotEmpty();
-
-        int prevPrice = Integer.MIN_VALUE;
-        for (ItemResponse item : items) {
-            assertThat(item.getPrice()).isGreaterThanOrEqualTo(prevPrice);
-            prevPrice = item.getPrice();
-        }
+        assertEquals(5, items.size());
+        assertEquals(items.get(0).getPrice(), 1500000);
     }
 
     @Test
     void findAllBySortPriceDescending() {
         //given
-        ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest4 = new ItemRequest("name", 40000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest5 = new ItemRequest("name", 50000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        itemService.save(itemRequest1);
-        itemService.save(itemRequest2);
-        itemService.save(itemRequest3);
-        itemService.save(itemRequest4);
-        itemService.save(itemRequest5);
+        init();
 
         //when
-        List<ItemResponse> items = itemService.findALlBySortPrice(false);
+        List<FindItemResponse> items = itemService.search(new FindItemRequest(null, null, null, null, false));
+
 
         //then
-        assertThat(items).isNotEmpty();
-
-        int prevPrice = Integer.MAX_VALUE;
-        for (ItemResponse item : items) {
-            assertThat(item.getPrice()).isLessThanOrEqualTo(prevPrice);
-            prevPrice = item.getPrice();
-        }
+        assertEquals(5, items.size());
+        assertEquals(items.get(0).getPrice(), 8500000);
     }
 
     @Test
     void findAllBySortBrandAndPriceAscending() {
         //given
-        ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest4 = new ItemRequest("name", 40000, 10, Category.ELECTRIC_GUITAR, Brand.SUHR);
-        ItemRequest itemRequest5 = new ItemRequest("name", 50000, 10, Category.ELECTRIC_GUITAR, Brand.PRS);
-        itemService.save(itemRequest1);
-        itemService.save(itemRequest2);
-        itemService.save(itemRequest3);
-        itemService.save(itemRequest4);
-        itemService.save(itemRequest5);
+        init();
 
         //when
-        List<ItemResponse> items = itemService.findAllBySortBrandAndPrice(true, Brand.JAMESTYLER);
+        List<FindItemResponse> items = itemService.search(new FindItemRequest(null, null, ELECTRIC_GUITAR, JAMESTYLER, true));
 
         //then
-        assertEquals(3, items.size());
-        for (int i = 0; i < items.size() - 1; i++) {
-            assertTrue(items.get(i).getPrice() <= items.get(i + 1).getPrice());
-        }
+        assertEquals(2, items.size());
+        assertEquals(items.get(0).getPrice(), 3500000);
+        assertEquals(items.get(1).getPrice(), 7500000);
     }
+
 
     @Test
     void findAllBySortBrandAndPriceDescending() {
         //given
-        ItemRequest itemRequest1 = new ItemRequest("name", 10000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest2 = new ItemRequest("name", 20000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest3 = new ItemRequest("name", 30000, 10, Category.ELECTRIC_GUITAR, Brand.JAMESTYLER);
-        ItemRequest itemRequest4 = new ItemRequest("name", 40000, 10, Category.ELECTRIC_GUITAR, Brand.SUHR);
-        ItemRequest itemRequest5 = new ItemRequest("name", 50000, 10, Category.ELECTRIC_GUITAR, Brand.PRS);
-        itemService.save(itemRequest1);
-        itemService.save(itemRequest2);
-        itemService.save(itemRequest3);
-        itemService.save(itemRequest4);
-        itemService.save(itemRequest5);
+        init();
 
         //when
-        List<ItemResponse> items = itemService.findAllBySortBrandAndPrice(false, Brand.JAMESTYLER);
+        List<FindItemResponse> items = itemService.search(new FindItemRequest(null, null, ELECTRIC_GUITAR, JAMESTYLER, false));
 
         //then
-        assertEquals(3, items.size());
-        for (int i = 0; i < items.size() - 1; i++) {
-            assertTrue(items.get(i).getPrice() >= items.get(i + 1).getPrice());
-        }
+        assertEquals(2, items.size());
+        assertEquals(items.get(0).getPrice(), 7500000);
+        assertEquals(items.get(1).getPrice(), 3500000);
+    }
+
+
+    private AddItemResponse addItem(String name, int price, int quantity, Category category, Brand brand) {
+        AddItemRequest request = AddItemRequest.builder()
+                .name(name)
+                .price(price)
+                .quantity(1)
+                .category(category)
+                .brand(brand)
+                .build();
+        return itemService.save(request);
+    }
+
+    private void init() {
+        addItem("제임스 타일러", 7500000, 1, ELECTRIC_GUITAR, JAMESTYLER);
+        addItem("존 써", 8500000, 1, ELECTRIC_GUITAR, SUHR);
+        addItem("펜더", 6500000, 1, ELECTRIC_GUITAR, FENDER);
+        addItem("PRS", 1500000, 1, ELECTRIC_GUITAR, PRS);
+        addItem("제임스 타일러", 3500000, 1, ELECTRIC_GUITAR, JAMESTYLER);
     }
 }
