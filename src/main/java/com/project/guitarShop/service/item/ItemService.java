900e9d6fc5f313b272478fb98e8e8141ae570dc5
songseongbin <ssbin0916@naver.com>
2024-05-07 15:23:42 +0900

    아이템 저장, 검색, 정렬 서비스

--- src/main/java/com/project/guitarShop/service/item/ItemService.java
+++ src/main/java/com/project/guitarShop/service/item/ItemService.java
@@ -1,67 +1,42 @@
 package com.project.guitarShop.service.item;
 
-import com.project.guitarShop.exception.NotFoundItemException;
-import com.project.guitarShop.domain.item.Brand;
 import com.project.guitarShop.domain.item.Item;
-import com.project.guitarShop.dto.item.ItemRequest;
-import com.project.guitarShop.dto.item.ItemResponse;
-import com.project.guitarShop.item.app.QItemResponse;
-import com.project.guitarShop.item.domain.QItem;
+import com.project.guitarShop.dto.item.ItemRequest.AddItemRequest;
+import com.project.guitarShop.dto.item.ItemRequest.FindItemRequest;
+import com.project.guitarShop.dto.item.ItemResponse.AddItemResponse;
+import com.project.guitarShop.dto.item.ItemResponse.FindItemResponse;
+import com.project.guitarShop.exception.item.ItemSaveException;
+import com.project.guitarShop.exception.item.NotFoundItemException;
 import com.project.guitarShop.repository.item.ItemRepository;
-import com.querydsl.core.types.Order;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class ItemService {
 
-    private final JPAQueryFactory queryFactory;
-    private final QItem qItem = QItem.item;
     private final ItemRepository itemRepository;
 
-    @Transactional
-    public ItemResponse save(ItemRequest itemRequest) {
-        Item item = Item.toDomain(itemRequest);
+    public AddItemResponse save(AddItemRequest request) {
+        if (request == null) {
+            throw new NotFoundItemException("입력된 아이템이 없습니다.");
+        }
 
-        itemRepository.save(item);
-
-        Long itemId = item.getId();
-        Item saveItem = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundItemException("아이템을 찾을 수 없습니다."));
-        return new ItemResponse(saveItem.getName(), saveItem.getPrice(), saveItem.getQuantity(), saveItem.getCategory(), saveItem.getBrand());
+        try {
+            Item save = itemRepository.save(request.toDomain());
+            return new AddItemResponse(save);
+        } catch (Exception e) {
+            throw new ItemSaveException("아이템 등록에 실패했습니다.");
+        }
     }
 
-    public List<ItemResponse> findALlBySortPrice(boolean ascending) {
-        OrderSpecifier<Integer> orderSpecifier = ascending ? qItem.price.asc() : qItem.price.desc();
-        return queryFactory
-                .select(new QItemResponse(
-                        qItem.name, qItem.price, qItem.quantity, qItem.category, qItem.brand))
-                .from(qItem)
-                .orderBy(orderSpecifier)
-                .fetch();
-    }
-
-    public List<ItemResponse> findAllBySortBrandAndPrice(boolean ascending, Brand brand) {
-        Order order = ascending ? Order.ASC : Order.DESC;
-
-        return queryFactory
-                .select(new QItemResponse(
-                        qItem.name, qItem.price, qItem.quantity, qItem.category, qItem.brand))
-                .from(qItem)
-                .where(qItem.brand.eq(brand))
-                .orderBy(qItem.brand.asc(), new OrderSpecifier<>(order, qItem.price))
-                .fetch();
-    }
-
-    public Optional<ItemResponse> findByName(String name) {
-        return itemRepository.findByName(name);
+    @Transactional(readOnly = true)
+    public List<FindItemResponse> search(FindItemRequest request) {
+        return itemRepository.search(request);
     }
 }
 
