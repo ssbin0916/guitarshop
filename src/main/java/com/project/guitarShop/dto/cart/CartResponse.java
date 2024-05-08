ad77d40aae02704cb7ffedfee039babee665f06d
songseongbin <ssbin0916@naver.com>
2024-05-08 12:06:39 +0900

    내부 클래스로 변환

--- src/main/java/com/project/guitarShop/dto/cart/CartResponse.java
+++ src/main/java/com/project/guitarShop/dto/cart/CartResponse.java
@@ -1,22 +1,46 @@
 package com.project.guitarShop.dto.cart;
 
-import com.project.guitarShop.domain.member.Member;
+import com.project.guitarShop.domain.cart.Cart;
 import com.project.guitarShop.domain.orderItem.OrderItem;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CartResponse {
 
     @Getter
     @Setter
-    public static class AddCartRequestDto {
+    public static class AddCartResponse {
 
+        private Long id;
+        private Long memberId;
+        private List<OrderItemResponse> orderItems;
+
+        public AddCartResponse(Cart cart) {
+            this.id = cart.getId();
+            this.orderItems = cart.getOrderItems().stream()
+                    .map(OrderItemResponse::new)
+                    .collect(Collectors.toList());
+        }
     }
 
-    private Long id;
-    private Member member;
-    private List<OrderItem> orderItems;
+    @Getter
+    @Setter
+    public static class OrderItemResponse {
+        private Long id;
+        private String itemName;
+        private int price;
+        private int count;
 
+        public OrderItemResponse(OrderItem orderItem) {
+            this.id = orderItem.getId();
+            this.itemName = orderItem.getItem().getName();
+            this.price = orderItem.getOrderPrice();
+            this.count = orderItem.getQuantity();
+        }
+    }
 }
+
+
