7ee84f0401fc9e945ac998977fb2d39406173399
songseongbin <ssbin0916@naver.com>
2024-05-08 12:05:48 +0900

    불필요한 클래스, 생성자 삭제

--- src/main/java/com/project/guitarShop/dto/cart/CartRequest.java
+++ src/main/java/com/project/guitarShop/dto/cart/CartRequest.java
@@ -1,6 +1,5 @@
 package com.project.guitarShop.dto.cart;
 
-import com.project.guitarShop.domain.cart.Cart;
 import com.project.guitarShop.domain.member.Member;
 import com.project.guitarShop.domain.orderItem.OrderItem;
 import lombok.Getter;
@@ -12,21 +11,8 @@ public class CartRequest {
 
     @Getter
     @Setter
-    public static class AddCartRequestDto {
+    public static class AddCartRequest {
         private Member member;
         private List<OrderItem> orderItems;
-
-        public Cart toDomain() {
-            return Cart.builder()
-                    .member(member)
-                    .orderItems(orderItems)
-                    .build();
-        }
-    }
-
-    @Getter
-    @Setter
-    public static class CartListRequestDto {
-
     }
 }
