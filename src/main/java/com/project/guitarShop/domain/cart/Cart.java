9c13940238c59c5d6223a895200964ff4030587a
songseongbin <ssbin0916@naver.com>
2024-05-08 12:03:53 +0900

    불필요한 생성자 삭제

--- src/main/java/com/project/guitarShop/domain/cart/Cart.java
+++ src/main/java/com/project/guitarShop/domain/cart/Cart.java
@@ -3,11 +3,11 @@ package com.project.guitarShop.domain.cart;
 import com.project.guitarShop.domain.member.Member;
 import com.project.guitarShop.domain.orderItem.OrderItem;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -25,19 +25,15 @@ public class Cart {
     private Member member;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<OrderItem> orderItems;
+    private List<OrderItem> orderItems = new ArrayList<>();
 
-    public Cart(Member member) {
-        this.member = member;
-    }
 
-    public void addItem(OrderItem item) {
-        orderItems.add(item);
-        item.setCart(this);
-    }
 
-    public void removeItem(OrderItem item) {
-        orderItems.remove(item);
-        item.setCart(null);
+
+    public static Cart addCart(Member member, List<OrderItem> orderItems) {
+        Cart cart = new Cart();
+        cart.member = member;
+        cart.orderItems = new ArrayList<>(orderItems);
+        return cart;
     }
 }
