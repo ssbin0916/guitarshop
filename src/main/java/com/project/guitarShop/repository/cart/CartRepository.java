5183efba898282d61881eb6f06d0453254030b93
songseongbin <ssbin0916@naver.com>
2024-05-08 12:12:49 +0900

    멤버 아이디 찾기 추가

--- src/main/java/com/project/guitarShop/repository/cart/CartRepository.java
+++ src/main/java/com/project/guitarShop/repository/cart/CartRepository.java
@@ -3,5 +3,10 @@ package com.project.guitarShop.repository.cart;
 import com.project.guitarShop.domain.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByMemberId(Long id);
+
 }
