76d870abd0fb255fd08d45fa4a9499a5af94a4ce
songseongbin <ssbin0916@naver.com>
2024-05-10 00:19:10 +0900

    Spring Security 적용

--- src/main/java/com/project/guitarShop/SecurityConfig.java
+++ src/main/java/com/project/guitarShop/SecurityConfig.java
@@ -1,19 +1,16 @@
 package com.project.guitarShop;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebConfig implements WebMvcConfigurer {
+public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -25,19 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
 
         httpSecurity
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 );
+
+        httpSecurity
+                .csrf((auth) -> auth.disable());
+
         return httpSecurity.build();
-    }
-
-    @PersistenceContext
-    private EntityManager em;
-
-    @Bean
-    public JPAQueryFactory queryFactory() {
-        return new JPAQueryFactory(em);
     }
 }
