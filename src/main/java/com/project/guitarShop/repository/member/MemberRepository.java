4f4bc6d64cc4c114d4394d49817417c57ba71818
songseongbin <ssbin0916@naver.com>
2024-05-10 00:19:49 +0900

    변수 이름 변경

--- src/main/java/com/project/guitarShop/repository/member/MemberRepository.java
+++ src/main/java/com/project/guitarShop/repository/member/MemberRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findByLoginId(String loginId);
+    Optional<Member> findByLoginEmail(String loginEmail);
 }
