4b6ac7561156a832e4408813fbd883ef77a27255
songseongbin <ssbin0916@naver.com>
2024-05-10 10:06:37 +0900

    inner class 임포트 수정

--- src/main/java/com/project/guitarShop/controller/member/ApiMemberController.java
+++ src/main/java/com/project/guitarShop/controller/member/ApiMemberController.java
@@ -1,10 +1,7 @@
 package com.project.guitarShop.controller.member;
 
-import com.project.guitarShop.dto.member.MemberRequest.JoinRequest;
-import com.project.guitarShop.dto.member.MemberRequest.LoginRequest;
-import com.project.guitarShop.dto.member.MemberRequest.UpdatePasswordRequest;
-import com.project.guitarShop.dto.member.MemberResponse.JoinResponse;
-import com.project.guitarShop.dto.member.MemberResponse.LoginResponse;
+import com.project.guitarShop.dto.member.MemberRequest.*;
+import com.project.guitarShop.dto.member.MemberResponse.*;
 import com.project.guitarShop.service.member.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -39,6 +36,14 @@ public class ApiMemberController {
     public ResponseEntity<String> updatePassword(@PathVariable Long id, @RequestBody @Valid UpdatePasswordRequest request) {
 
         memberService.updatePassword(id, request);
+
+        return ResponseEntity.ok("변경 완료");
+    }
+
+    @PutMapping("/{id}/updateInfo")
+    public ResponseEntity<String> updateInfo(@PathVariable Long id, @RequestBody @Valid UpdateInfoRequest request) {
+
+        memberService.updateInfo(id, request);
 
         return ResponseEntity.ok("변경 완료");
     }
