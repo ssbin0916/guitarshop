0d399dc28d08fc7086d36bf07593c363bd65c899
songseongbin <ssbin0916@naver.com>
2024-05-11 11:37:14 +0900

    검증, 오류 메시지 추가

--- src/main/java/com/project/guitarShop/controller/item/ApiItemController.java
+++ src/main/java/com/project/guitarShop/controller/item/ApiItemController.java
@@ -2,6 +2,7 @@ package com.project.guitarShop.controller.item;
 
 import com.project.guitarShop.dto.item.ItemRequest.*;
 import com.project.guitarShop.dto.item.ItemResponse.*;
+import com.project.guitarShop.exception.item.NotFoundItemException;
 import com.project.guitarShop.service.item.ItemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -20,10 +23,30 @@ public class ApiItemController {
     private final ItemService itemService;
 
     @PostMapping("/addItem")
-    public ResponseEntity<AddItemResponse> save(@Valid @RequestBody AddItemRequest request) {
+    public ResponseEntity<?> save(@Valid @RequestBody AddItemRequest request) {
 
-        AddItemResponse response = itemService.save(request);
+        try {
+            AddItemResponse response = itemService.save(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("서버 오류기 발생했습니다.");
+        }
+    }
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    @PostMapping("/search")
+    public ResponseEntity<?> search(@Valid @RequestBody FindItemRequest request) {
+
+        try {
+            List<FindItemResponse> response = itemService.search(request);
+            return ResponseEntity.ok().body(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (NotFoundItemException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다.");
+        }
     }
 }
