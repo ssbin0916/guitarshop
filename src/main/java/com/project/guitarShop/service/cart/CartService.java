6f4ebc709aebaff1c31b3a80f51186b35c207e2a
songseongbin <ssbin0916@naver.com>
2024-05-12 11:41:57 +0900

    리턴 타입 dto로 변경

--- src/main/java/com/project/guitarShop/service/cart/CartService.java
+++ src/main/java/com/project/guitarShop/service/cart/CartService.java
@@ -3,10 +3,10 @@ package com.project.guitarShop.service.cart;
 import com.project.guitarShop.domain.cart.Cart;
 import com.project.guitarShop.domain.member.Member;
 import com.project.guitarShop.domain.order.Order;
-import com.project.guitarShop.domain.orderItem.OrderItem;
+import com.project.guitarShop.dto.cart.CartRequest.AddCartRequest;
+import com.project.guitarShop.dto.cart.CartResponse.AddCartResponse;
 import com.project.guitarShop.exception.NotFoundMemberException;
 import com.project.guitarShop.exception.cart.NotFoundCartException;
-import com.project.guitarShop.exception.item.NotFoundItemException;
 import com.project.guitarShop.exception.order.NotFoundOrderException;
 import com.project.guitarShop.repository.cart.CartRepository;
 import com.project.guitarShop.repository.member.MemberRepository;
@@ -15,8 +15,6 @@ import com.project.guitarShop.repository.orderItem.OrderItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,19 +26,23 @@ public class CartService {
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
 
-    public Cart addCart(Long memberId, Long orderId, List<OrderItem> orderItems) {
+    public AddCartResponse addCart(AddCartRequest request) {
 
-        Member member = memberRepository.findById(memberId)
+        Member member = memberRepository.findById(request.getMemberId())
                 .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
 
-        Order order = orderRepository.findById(orderId)
+        Order order = orderRepository.findById(request.getOrderId())
                 .orElseThrow(() -> new NotFoundOrderException("해당 주문을 찾을 수 없습니다."));
 
-        Cart cart = Cart.addCart(member, orderItems);
+        Cart cart = Cart.builder()
+                .member(member)
+                .order(order)
+                .orderItems(request.getOrderItems())
+                .build();
 
-        cartRepository.save(cart);
+        Cart save = cartRepository.save(cart);
 
-        return cart;
+        return new AddCartResponse(save);
     }
 
     public void removeItemFromCart(Long cartId) {
