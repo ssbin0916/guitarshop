92ce5d553ef61672c7665ffc4c7a39edc6ad3bf9
songseongbin <ssbin0916@naver.com>
2024-05-12 11:42:51 +0900

    주문 취소, 장바구니에서 주문 서비스 추가

--- src/main/java/com/project/guitarShop/service/order/OrderService.java
+++ src/main/java/com/project/guitarShop/service/order/OrderService.java
@@ -1,6 +1,7 @@
 package com.project.guitarShop.service.order;
 
 import com.project.guitarShop.domain.address.Address;
+import com.project.guitarShop.domain.cart.Cart;
 import com.project.guitarShop.domain.delivery.Delivery;
 import com.project.guitarShop.domain.delivery.DeliveryStatus;
 import com.project.guitarShop.domain.item.Item;
@@ -8,11 +9,14 @@ import com.project.guitarShop.domain.member.Member;
 import com.project.guitarShop.domain.order.Order;
 import com.project.guitarShop.domain.order.OrderStatus;
 import com.project.guitarShop.domain.orderItem.OrderItem;
-import com.project.guitarShop.dto.order.OrderRequest.CreateOrderRequest;
-import com.project.guitarShop.dto.order.OrderResponse.CreateOrdersResponse;
+import com.project.guitarShop.dto.order.OrderRequest.*;
+import com.project.guitarShop.dto.order.OrderResponse.*;
 import com.project.guitarShop.exception.NotEnoughStockException;
 import com.project.guitarShop.exception.NotFoundMemberException;
+import com.project.guitarShop.exception.cart.NotFoundCartException;
 import com.project.guitarShop.exception.item.NotFoundItemException;
+import com.project.guitarShop.exception.order.NotFoundOrderException;
+import com.project.guitarShop.exception.order.OrderCancelException;
 import com.project.guitarShop.repository.cart.CartRepository;
 import com.project.guitarShop.repository.item.ItemRepository;
 import com.project.guitarShop.repository.member.MemberRepository;
@@ -55,6 +59,7 @@ public class OrderService {
                 .orderPrice(item.getPrice())
                 .quantity(request.getQuantity())
                 .build();
+
         List<OrderItem> orderItems = new ArrayList<>();
         orderItems.add(orderItem);
 
@@ -75,28 +80,63 @@ public class OrderService {
     }
 
 
-//    public Order cancelOrder(Long orderId) {
-//        Order order = orderRepository.findById(orderId)
-//                .orElseThrow(() -> new NotFoundOrderException("해당 주문을 찾을 수 없습니다."));
-//        order.cancel();
-//        return orderRepository.save(order);
-//    }
-//
-//    public Long orderFromCart(Long memberId) {
-//        Cart cart = cartRepository.findByMemberId(memberId)
-//                .orElseThrow(() -> new NotFoundCartException("장바구니가 비어있습니다."));
-//
-//        if (cart.getOrderItems().isEmpty()) {
-//            throw new NotFoundCartException("장바구니에 주문할 상품이 없습니다.");
-//        }
-//
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
-//        Delivery delivery = new Delivery(member.getAddress());
-//
-//        List<OrderItem> orderItems = new ArrayList<>(cart.getOrderItems());
-//
-//        orderRepository.save(order);
-//        return order.getId();
-//    }
+    public void cancelOrder(Long orderId) {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new NotFoundOrderException("해당 주문을 찾을 수 없습니다."));
+
+        if (order.getOrderStatus() == OrderStatus.CANCEL) {
+            throw new OrderCancelException("이미 취소된 주문입니다.");
+        }
+
+        if (order.getDelivery().getStatus() == DeliveryStatus.DELIVERING || order.getDelivery().getStatus() == DeliveryStatus.COMPLETE) {
+            throw new OrderCancelException("배송된 주문은 취소할 수 없습니다.");
+        }
+
+        order.cancel();
+
+        orderRepository.save(order);
+    }
+
+    public OrderFormCartResponse orderFromCart(OrderFormCartRequest request) {
+
+        Cart cart = cartRepository.findById(request.getCartId())
+                .orElseThrow(() -> new NotFoundCartException("장바구니가 비어있습니다."));
+
+        Member member = memberRepository.findById(request.getMemberId())
+                .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
+
+        Item item = itemRepository.findById(request.getItemId())
+                .orElseThrow(() -> new NotFoundItemException("해당 상품을 찾을 수 없습니다."));
+
+        if (cart.getOrderItems().isEmpty()) {
+            throw new NotFoundCartException("장바구니에 주문할 상품이 없습니다.");
+        }
+
+        Address deliveryAddress = (request.getDeliveryAddress() != null ? request.getDeliveryAddress() : member.getAddress());
+        Delivery delivery = new Delivery(deliveryAddress, DeliveryStatus.READY);
+
+        OrderItem orderItem = OrderItem.builder()
+                .item(item)
+                .orderPrice(item.getPrice())
+                .quantity(request.getQuantity())
+                .build();
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        orderItems.add(orderItem);
+
+        Order order = Order.builder()
+                .member(member)
+                .orderItems(orderItems)
+                .delivery(delivery)
+                .orderDate(LocalDateTime.now())
+                .orderStatus(OrderStatus.ORDER)
+                .build();
+
+        orderRepository.save(order);
+        item.removeStock(request.getQuantity());
+        itemRepository.save(item);
+
+        return new OrderFormCartResponse(order);
+    }
 }
