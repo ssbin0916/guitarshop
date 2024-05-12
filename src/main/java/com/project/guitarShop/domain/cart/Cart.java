7b702d78dc930736d7d6dcd3b1923390ef04f7df
songseongbin <ssbin0916@naver.com>
2024-05-12 11:35:37 +0900

    연관 관계 매핑

--- src/main/java/com/project/guitarShop/domain/cart/Cart.java
+++ src/main/java/com/project/guitarShop/domain/cart/Cart.java
@@ -1,8 +1,10 @@
 package com.project.guitarShop.domain.cart;
 
 import com.project.guitarShop.domain.member.Member;
+import com.project.guitarShop.domain.order.Order;
 import com.project.guitarShop.domain.orderItem.OrderItem;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -24,16 +26,16 @@ public class Cart {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @OneToOne(mappedBy = "cart")
+    private Order order;
+
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-
-
-
-    public static Cart addCart(Member member, List<OrderItem> orderItems) {
-        Cart cart = new Cart();
-        cart.member = member;
-        cart.orderItems = new ArrayList<>(orderItems);
-        return cart;
+    @Builder
+    public Cart(Member member, Order order, List<OrderItem> orderItems) {
+        this.member = member;
+        this.order = order;
+        this.orderItems = new ArrayList<>(orderItems);
     }
 }
