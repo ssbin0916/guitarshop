15057a64ec1f39d401a892b012b196b37a0f0448
songseongbin <ssbin0916@naver.com>
2024-05-13 21:54:44 +0900

    파리미터 이름 변경

--- src/main/java/com/project/guitarShop/repository/cart/CartRepository.java
+++ src/main/java/com/project/guitarShop/repository/cart/CartRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    Optional<Cart> findByMemberId(Long id);
+    Optional<Cart> findByMemberId(Long memberId);
 }
