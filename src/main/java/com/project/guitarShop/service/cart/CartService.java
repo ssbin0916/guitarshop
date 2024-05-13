b9e4d559ded6791a8e72ef9d6709ece6a51de822
songseongbin <ssbin0916@naver.com>
2024-05-13 16:59:52 +0900

    장바구니 담기 서비스 수정, 장바구니 삭제 추가

--- src/main/java/com/project/guitarShop/service/cart/CartService.java
+++ src/main/java/com/project/guitarShop/service/cart/CartService.java
@@ -1,16 +1,17 @@
 package com.project.guitarShop.service.cart;
 
+import com.project.guitarShop.dto.cart.CartRequest.*;
+import com.project.guitarShop.dto.cart.CartResponse.*;
 import com.project.guitarShop.entity.cart.Cart;
 import com.project.guitarShop.entity.cartItem.CartItem;
 import com.project.guitarShop.entity.item.Item;
 import com.project.guitarShop.entity.member.Member;
-import com.project.guitarShop.dto.cart.CartRequest.AddCartRequest;
-import com.project.guitarShop.dto.cart.CartResponse.AddCartResponse;
 import com.project.guitarShop.exception.NotEnoughStockException;
 import com.project.guitarShop.exception.NotFoundMemberException;
 import com.project.guitarShop.exception.cart.NotFoundCartException;
 import com.project.guitarShop.exception.item.NotFoundItemException;
 import com.project.guitarShop.repository.cart.CartRepository;
+import com.project.guitarShop.repository.cartItem.CartItemRepository;
 import com.project.guitarShop.repository.item.ItemRepository;
 import com.project.guitarShop.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -34,55 +35,42 @@ public class CartService {
         Member member = memberRepository.findById(request.getMemberId())
                 .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
 
-        Cart cart = cartRepository.findByMemberId(request.getMemberId())
-                .orElseGet(() -> Cart.builder().member(member).build());
-
         Item item = itemRepository.findById(request.getItemId())
                 .orElseThrow(() -> new NotFoundItemException("해당 상품을 찾을 수 없습니다."));
 
-        if (item.getQuantity() < request.getQuantity()) {
+        if (item.getQuantity() == 0) {
             throw new NotEnoughStockException("상품 재고가 부족합니다.");
         }
 
         CartItem cartItem = CartItem.builder()
-                .cart(cart)
                 .item(item)
-                .quantity(request.getQuantity())
+                .name(item.getName())
+                .price(item.getPrice())
                 .build();
 
         List<CartItem> cartItems = new ArrayList<>();
         cartItems.add(cartItem);
 
-        Cart save = Cart.builder()
+        Cart cart = Cart.builder()
                 .member(member)
                 .cartItems(cartItems)
+                .price(cartItem.getPrice())
                 .build();
 
-        return new AddCartResponse(save);
+        cartRepository.save(cart);
+        item.removeStock(1);
+        itemRepository.save(item);
+
+        return new AddCartResponse(cart);
     }
 
-    public void removeItemFromCart(Long cartId) {
+    public void remove(Long cartId) {
 
-        cartRepository.findById(cartId)
+        Cart cart = cartRepository.findById(cartId)
                 .orElseThrow(() -> new NotFoundCartException("해당 장바구니를 찾을 수 없습니다."));
+
+        cart.remove();
 
         cartRepository.deleteById(cartId);
     }
-
-//    public Cart updateItemQuantity(Long cartId, Long orderItemId, int quantity) {
-//
-//        Cart cart = cartRepository.findById(cartId)
-//                .orElseThrow(() -> new NotFoundCartException("해당 장바구니를 찾을 수 없습니다."));
-//
-//        OrderItem orderItem = cart.getOrderItems().stream()
-//                .filter(item -> item.getId().equals(orderItemId))
-//                .findFirst()
-//                .orElseThrow(() -> new NotFoundItemException("해당 주문 상품을 찾을 수 없습니다."));
-//
-//        orderItem.setQuantity(quantity);
-//        orderItemRepository.save(orderItem);
-//        cartRepository.save(cart);
-//
-//        return cart;
-//    }
 }
