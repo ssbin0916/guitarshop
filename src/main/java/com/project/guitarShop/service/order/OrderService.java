302c36756bd7e304642c0d0ee55b38d932c46bd0
songseongbin <ssbin0916@naver.com>
2024-05-13 21:55:30 +0900

    수량 검증 변경

--- src/main/java/com/project/guitarShop/service/order/OrderService.java
+++ src/main/java/com/project/guitarShop/service/order/OrderService.java
@@ -1,6 +1,10 @@
 package com.project.guitarShop.service.order;
 
+import com.project.guitarShop.dto.order.OrderRequest.*;
+import com.project.guitarShop.dto.order.OrderResponse.*;
 import com.project.guitarShop.entity.address.Address;
+import com.project.guitarShop.entity.cart.Cart;
+import com.project.guitarShop.entity.cartItem.CartItem;
 import com.project.guitarShop.entity.delivery.Delivery;
 import com.project.guitarShop.entity.delivery.DeliveryStatus;
 import com.project.guitarShop.entity.item.Item;
@@ -8,14 +12,14 @@ import com.project.guitarShop.entity.member.Member;
 import com.project.guitarShop.entity.order.Order;
 import com.project.guitarShop.entity.order.OrderStatus;
 import com.project.guitarShop.entity.orderItem.OrderItem;
-import com.project.guitarShop.dto.order.OrderRequest.*;
-import com.project.guitarShop.dto.order.OrderResponse.*;
 import com.project.guitarShop.exception.NotEnoughStockException;
 import com.project.guitarShop.exception.NotFoundMemberException;
+import com.project.guitarShop.exception.cart.NotFoundCartException;
 import com.project.guitarShop.exception.item.NotFoundItemException;
 import com.project.guitarShop.exception.order.NotFoundOrderException;
 import com.project.guitarShop.exception.order.OrderCancelException;
 import com.project.guitarShop.repository.cart.CartRepository;
+import com.project.guitarShop.repository.cartItem.CartItemRepository;
 import com.project.guitarShop.repository.item.ItemRepository;
 import com.project.guitarShop.repository.member.MemberRepository;
 import com.project.guitarShop.repository.order.OrderRepository;
@@ -36,6 +40,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final OrderRepository orderRepository;
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
 
     public CreateOrdersResponse order(CreateOrderRequest request) {
 
@@ -45,7 +50,7 @@ public class OrderService {
         Item item = itemRepository.findById(request.getItemId())
                 .orElseThrow(() -> new NotFoundItemException("해당 상품을 찾을 수 없습니다."));
 
-        if (item.getQuantity() < request.getQuantity()) {
+        if (item.getQuantity() == 0) {
             throw new NotEnoughStockException("상품의 재고가 부족합니다.");
         }
 
@@ -54,8 +59,8 @@ public class OrderService {
 
         OrderItem orderItem = OrderItem.builder()
                 .item(item)
-                .orderPrice(item.getPrice())
-                .quantity(request.getQuantity())
+                .name(item.getName())
+                .price(item.getPrice())
                 .build();
 
         List<OrderItem> orderItems = new ArrayList<>();
@@ -69,9 +74,8 @@ public class OrderService {
                 .orderStatus(OrderStatus.ORDER)
                 .build();
 
-
         orderRepository.save(order);
-        item.removeStock(request.getQuantity());
+        item.removeStock(1);
         itemRepository.save(item);
 
         return new CreateOrdersResponse(order);
@@ -96,45 +100,34 @@ public class OrderService {
         orderRepository.save(order);
     }
 
-//    public OrderFormCartResponse orderFromCart(OrderFormCartRequest request) {
-//
-//        Cart cart = cartRepository.findById(request.getCartId())
-//                .orElseThrow(() -> new NotFoundCartException("장바구니가 비어있습니다."));
-//
-//        Member member = memberRepository.findById(request.getMemberId())
-//                .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
-//
-//        Item item = itemRepository.findById(request.getItemId())
-//                .orElseThrow(() -> new NotFoundItemException("해당 상품을 찾을 수 없습니다."));
-//
-//        if (cart.getOrderItems().isEmpty()) {
-//            throw new NotFoundCartException("장바구니에 주문할 상품이 없습니다.");
-//        }
-//
-//        Address deliveryAddress = (request.getDeliveryAddress() != null ? request.getDeliveryAddress() : member.getAddress());
-//        Delivery delivery = new Delivery(deliveryAddress, DeliveryStatus.READY);
-//
-//        OrderItem orderItem = OrderItem.builder()
-//                .item(item)
-//                .orderPrice(item.getPrice())
-//                .quantity(request.getQuantity())
-//                .build();
-//
-//        List<OrderItem> orderItems = new ArrayList<>();
-//        orderItems.add(orderItem);
-//
-//        Order order = Order.builder()
-//                .member(member)
-//                .orderItems(orderItems)
-//                .delivery(delivery)
-//                .orderDate(LocalDateTime.now())
-//                .orderStatus(OrderStatus.ORDER)
-//                .build();
-//
-//        orderRepository.save(order);
-//        item.removeStock(request.getQuantity());
-//        itemRepository.save(item);
-//
-//        return new OrderFormCartResponse(order);
-//    }
+    public OrderFormCartResponse orderFromCart(OrderFormCartRequest request) {
+
+        Cart cart = cartRepository.findById(request.getCartId())
+                .orElseThrow(() -> new NotFoundCartException("장바구니가 비어있습니다."));
+
+        Member member = memberRepository.findById(cart.getId())
+                .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
+
+        CartItem cartItem = cartItemRepository.findById(cart.getCartItems().get(0).getId())
+                .orElseThrow(() -> new NotFoundItemException("해당 아이템을 찾을 수 없습니다."));
+
+        Address deliveryAddress = (request.getDeliveryAddress() != null ? request.getDeliveryAddress() : member.getAddress());
+        Delivery delivery = new Delivery(deliveryAddress, DeliveryStatus.READY);
+
+        List<CartItem> cartItems = new ArrayList<>();
+        cartItems.add(cartItem);
+
+        Order order = Order.builder()
+                .member(member)
+                .cartItems(cartItems)
+                .delivery(delivery)
+                .orderDate(LocalDateTime.now())
+                .orderStatus(OrderStatus.ORDER)
+                .build();
+
+        orderRepository.save(order);
+
+        return new OrderFormCartResponse(order);
+    }
+
 }
