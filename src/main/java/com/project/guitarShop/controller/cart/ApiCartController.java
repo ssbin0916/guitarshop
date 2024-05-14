0563ecc23ac8a813ce25b6d3fb4c6d9e4e942063
songseongbin <ssbin0916@naver.com>
2024-05-15 00:30:25 +0900

    글로벌 예외 처리로 코드 간소화

--- src/main/java/com/project/guitarShop/controller/cart/ApiCartController.java
+++ src/main/java/com/project/guitarShop/controller/cart/ApiCartController.java
@@ -1,17 +1,13 @@
 package com.project.guitarShop.controller.cart;
 
-import com.project.guitarShop.dto.cart.CartRequest.AddCartRequest;
-import com.project.guitarShop.dto.cart.CartResponse.AddCartResponse;
-import com.project.guitarShop.exception.NotEnoughStockException;
-import com.project.guitarShop.exception.NotFoundMemberException;
-import com.project.guitarShop.exception.cart.NotFoundCartException;
-import com.project.guitarShop.exception.item.NotFoundItemException;
 import com.project.guitarShop.service.cart.CartService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
@@ -21,28 +17,17 @@ public class ApiCartController {
     private final CartService cartService;
 
     @PostMapping("/{memberId}/{itemId}/addCart")
-    public ResponseEntity<?> cart(@Valid @RequestBody AddCartRequest request) {
+    public ResponseEntity<Long> addCart(@PathVariable Long memberId, @PathVariable Long itemId) {
 
-        try {
-            AddCartResponse response = cartService.addCart(request);
-            return ResponseEntity.ok(response);
-        } catch (NotFoundMemberException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 회원을 찾을 수 없습니다.");
-        } catch (NotFoundItemException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 상품을 찾을 수 없습니다.");
-        } catch (NotEnoughStockException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("재고가 부족합니다.");
-        }
+        Long addCart = cartService.addCart(memberId, itemId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(addCart);
     }
 
     @PostMapping("/{cartId}/remove")
     public ResponseEntity<?> remove(@PathVariable Long cartId) {
 
-        try {
-            cartService.remove(cartId);
-            return ResponseEntity.ok("장바구니에 담긴 상품이 모두 삭제되었습니다.");
-        } catch (NotFoundCartException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 장바구니를 찾을 수 없습니다.");
-        }
+        cartService.remove(cartId);
+        return ResponseEntity.ok("장바구니에 담긴 상품이 모두 삭제되었습니다.");
     }
+
 }
--- src/main/java/com/project/guitarShop/controller/order/ApiOrderController.java
+++ src/main/java/com/project/guitarShop/controller/order/ApiOrderController.java
@@ -1,18 +1,13 @@
 package com.project.guitarShop.controller.order;
 
-import com.project.guitarShop.dto.order.OrderRequest.*;
-import com.project.guitarShop.dto.order.OrderResponse.*;
-import com.project.guitarShop.exception.NotEnoughStockException;
-import com.project.guitarShop.exception.NotFoundMemberException;
-import com.project.guitarShop.exception.cart.NotFoundCartException;
-import com.project.guitarShop.exception.item.NotFoundItemException;
-import com.project.guitarShop.exception.order.NotFoundOrderException;
 import com.project.guitarShop.service.order.OrderService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
@@ -22,46 +17,25 @@ public class ApiOrderController {
     private final OrderService orderService;
 
     @PostMapping("/{memberId}/{itemId}/order")
-    public ResponseEntity<?> order(@Valid @RequestBody CreateOrderRequest request) {
+    public ResponseEntity<Long> order(@PathVariable Long memberId, @PathVariable Long itemId) {
 
-        try {
-            CreateOrdersResponse response = orderService.order(request);
-            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        } catch (NotFoundMemberException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 회원을 찾을 수 없습니다.");
-        } catch (NotFoundItemException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 상품을 찾을 수 없습니다.");
-        } catch (NotEnoughStockException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("재고가 부족합니다.");
-        }
+        Long order = orderService.order(memberId, itemId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 
-    @PostMapping("/{orderId}/cancel")
-    public ResponseEntity<String> cancel(@PathVariable Long orderId) {
+//        @PostMapping("/{orderId}/cancel")
+//    public ResponseEntity<String> cancel(@PathVariable Long orderId) {
+//
+//        orderService.cancelOrder(orderId);
+//        return ResponseEntity.ok("주문이 취소되었습니다.");
+//
+//    }
 
-        try {
-            orderService.cancelOrder(orderId);
-            return ResponseEntity.ok("주문이 취소되었습니다.");
-        } catch (NotFoundOrderException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 주문을 찾을 수 없습니다.");
-        }
+    @PostMapping("/{cartId}/orderFromCart")
+    public ResponseEntity<Long> orderFormCart(@PathVariable Long cartId) {
+
+        Long order = orderService.orderFromCart(cartId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 
-    @PostMapping("/{cartId}/{cartItemId}/orderFromCart")
-    public ResponseEntity<?> orderFormCart(@PathVariable Long cartId, @PathVariable Long cartItemId, @Valid @RequestBody OrderFormCartRequest request) {
-
-        try {
-            OrderFormCartResponse response = orderService.orderFromCart(request);
-            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        } catch (NotFoundCartException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("카트에 상품이 없습니다.");
-        } catch (NotFoundMemberException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 회원을 찾을 수 없습니다.");
-        } catch (NotFoundItemException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 상품을 찾을 수 없습니다.");
-        }
-//        catch (NotEnoughStockException e) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("재고가 부족합니다.");
-//        }
-    }
 }
