6c4cd632df0e600b93ca7c053f5f62cf26ab63c7
songseongbin <ssbin0916@naver.com>
2024-05-17 14:51:42 +0900

    게시판, 댓글 레파지토리 생성

--- src/main/java/com/project/guitarShop/repository/board/BoardRepository.java
+++ src/main/java/com/project/guitarShop/repository/board/BoardRepository.java
@@ -4,5 +4,4 @@ import com.project.guitarShop.entity.board.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-
 }
