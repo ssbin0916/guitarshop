64c101ae1749c08e3f55d69d1c07d694a9b44d8d
songseongbin <ssbin0916@naver.com>
2024-07-05 04:14:40 +0900

    게시판, 댓글 이름 변경

--- src/main/java/com/project/guitarShop/repository/board/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.guitarShop.repository.board;
-
-import com.project.guitarShop.entity.board.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
---
+++ src/main/java/com/project/guitarShop/repository/reply/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.project.guitarShop.repository.reply;
+
+import com.project.guitarShop.entity.reply.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
