3a2f2bd849f5aa8d47c0ac18006c0dcc061d76a1
songseongbin <ssbin0916@naver.com>
2024-07-05 07:59:41 +0900

    락 획득, 제거 레파지토리

--- src/main/java/com/project/guitarShop/repository/redis/RedisRepository.java
+++ src/main/java/com/project/guitarShop/repository/redis/RedisRepository.java
@@ -1,35 +1,28 @@
 package com.project.guitarShop.repository.redis;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.time.Duration;
 
 @Repository
+@RequiredArgsConstructor
 public class RedisRepository {
 
-    private RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    public RedisRepository(RedisTemplate<String, String> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
-
-    public Boolean lock(String key) {
+    public Boolean lock(Long key) {
         return redisTemplate
                 .opsForValue()
-                .setIfAbsent(generateKey(key), "lock", Duration.ofMillis(3000));
+                .setIfAbsent(generateKey(key), "lock", Duration.ofMillis(3_000));
     }
 
-    public Boolean unlock(String key) {
+    public Boolean unlock(Long key) {
         return redisTemplate.delete(generateKey(key));
     }
 
-    private String generateKey(String key) {
+    private String generateKey(Long key) {
         return key.toString();
-    }
-
-    public Long increasesViewCount(Long key) {
-        String redisKey = String.valueOf(key);
-        return redisTemplate.opsForValue().increment(redisKey);
     }
 }
