9b483808bf6ba6b5c37b9a1fbe37b7b83f7a1071
songseongbin <ssbin0916@naver.com>
2024-07-05 07:59:09 +0900

    분산 락 재고 감소 서비스 통합

--- src/main/java/com/project/guitarShop/service/order/OrderService.java
+++ src/main/java/com/project/guitarShop/service/order/OrderService.java
@@ -1,10 +1,12 @@
 package com.project.guitarShop.service.order;
 
+import com.project.guitarShop.dto.order.OrderResponse.*;
+
 public interface OrderService {
 
-    Long order(Long memberId, Long itemId);
+    CreateOrderResponse order(Long memberId, Long itemId) throws InterruptedException;
 
-    void cancelOrder(Long orderId);
+    void cancel(Long id) throws InterruptedException;
 
-    Long orderFromCart(Long cartId);
+    CreateOrderFromCartResponse orderFromCart(Long cartId) throws InterruptedException;
 }
--- src/main/java/com/project/guitarShop/service/order/OrderServiceImpl.java
+++ src/main/java/com/project/guitarShop/service/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.project.guitarShop.service.order;
 
+import com.project.guitarShop.dto.order.OrderResponse.*;
 import com.project.guitarShop.entity.cart.Cart;
 import com.project.guitarShop.entity.cartItem.CartItem;
 import com.project.guitarShop.entity.delivery.Delivery;
@@ -18,6 +19,7 @@ import com.project.guitarShop.repository.cart.CartRepository;
 import com.project.guitarShop.repository.item.ItemRepository;
 import com.project.guitarShop.repository.member.MemberRepository;
 import com.project.guitarShop.repository.order.OrderRepository;
+import com.project.guitarShop.repository.redis.RedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,18 +28,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final ItemRepository itemRepository;
     private final OrderRepository orderRepository;
     private final CartRepository cartRepository;
+    private final RedisRepository redisRepository;
 
-    @Transactional
     @Override
-    public Long order(Long memberId, Long itemId) {
+    public CreateOrderResponse order(Long memberId, Long itemId) throws InterruptedException {
+
+        while (!redisRepository.lock(itemId)) {
+            Thread.sleep(100);
+        }
 
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
@@ -50,20 +56,27 @@ public class OrderServiceImpl implements OrderService {
                 .deliveryStatus(DeliveryStatus.READY)
                 .build();
 
-        OrderItem orderItem = OrderItem.createOrderItem(item, item.getName(), item.getPrice());
+        Order order = new Order();
 
-        Order order = Order.createOrder(member, delivery, orderItem);
+        try {
+            OrderItem orderItem = OrderItem.createOrderItem(item, item.getName(), item.getPrice());
+            Order.createOrder(member, delivery, orderItem);
+            orderRepository.save(order);
+        } finally {
+            redisRepository.unlock(itemId);
+        }
 
-        orderRepository.save(order);
-
-        return order.getId();
+        return new CreateOrderResponse(order);
     }
 
-    @Transactional
     @Override
-    public void cancelOrder(Long orderId) {
+    public void cancel(Long id) throws InterruptedException {
 
-        Order order = orderRepository.findById(orderId)
+        while (!redisRepository.unlock(id)) {
+            Thread.sleep(100);
+        }
+
+        Order order = orderRepository.findById(id)
                 .orElseThrow(() -> new NotFoundOrderException("해당 주문을 찾을 수 없습니다."));
 
         if (order.getOrderStatus() == OrderStatus.CANCEL) {
@@ -74,14 +87,20 @@ public class OrderServiceImpl implements OrderService {
             throw new OrderCancelException("배송된 주문은 취소할 수 없습니다.");
         }
 
-        order.cancel();
-
-        orderRepository.save(order);
+        try {
+            order.cancel();
+            orderRepository.save(order);
+        } finally {
+            redisRepository.unlock(id);
+        }
     }
 
-    @Transactional
     @Override
-    public Long orderFromCart(Long cartId) {
+    public CreateOrderFromCartResponse orderFromCart(Long cartId) throws InterruptedException {
+
+        while (!redisRepository.unlock(cartId)) {
+            Thread.sleep(100);
+        }
 
         Cart cart = cartRepository.findById(cartId)
                 .orElseThrow(() -> new NotFoundCartException("장바구니를 찾을 수 없습니다."));
@@ -94,6 +113,7 @@ public class OrderServiceImpl implements OrderService {
                 .deliveryStatus(DeliveryStatus.READY)
                 .build();
 
+
         List<CartItem> cartItems = cart.getCartItems();
         List<OrderItem> orderItems = new ArrayList<>();
 
@@ -102,11 +122,15 @@ public class OrderServiceImpl implements OrderService {
             orderItems.add(orderItem);
         }
 
-        Order order = Order.createOrder(member, delivery, orderItems.toArray(new OrderItem[0]));
+        Order order = new Order();
 
-        orderRepository.save(order);
-
-        return order.getId();
+        try {
+            Order.createOrder(member, delivery, orderItems.toArray(new OrderItem[0]));
+            orderRepository.save(order);
+        } finally {
+            redisRepository.unlock(cartId);
+        }
+        return new CreateOrderFromCartResponse(order);
     }
 
 }
