dbb720e234e8b5f5507960ac7b3351762598e77e
songseongbin <ssbin0916@naver.com>
2024-07-05 04:14:49 +0900

    게시판, 댓글 이름 변경

--- src/main/java/com/project/guitarShop/service/comment/CommentService.java
+++
@@ -1,9 +0,0 @@
-package com.project.guitarShop.service.comment;
-
-import com.project.guitarShop.dto.comment.CommentRequest.*;
-import com.project.guitarShop.dto.comment.CommentResponse.*;
-
-public interface CommentService {
-
-    CommentWriteResponse comment(CommentWriteRequest request);
-}
