26ec6dbda380af506e8b16ad722c5e4aac5ea82a
songseongbin <ssbin0916@naver.com>
2024-07-05 03:54:24 +0900

    게시판 엔티티 이름 변경, 조회 수 필드 추가

--- src/main/java/com/project/guitarShop/entity/post/Post.java
+++ src/main/java/com/project/guitarShop/entity/post/Post.java
@@ -1,7 +1,7 @@
-package com.project.guitarShop.entity.board;
+package com.project.guitarShop.entity.post;
 
 import com.project.guitarShop.entity.BaseEntity;
-import com.project.guitarShop.entity.comment.Comment;
+import com.project.guitarShop.entity.reply.Reply;
 import com.project.guitarShop.entity.member.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -18,14 +18,16 @@ import static jakarta.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Board extends BaseEntity {
+public class Post extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String content;
 
     private Integer view;
@@ -34,16 +36,16 @@ public class Board extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "board", cascade = ALL)
-    @OrderBy("id asc")
-    private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "post", cascade = ALL)
+    private List<Reply> replies = new ArrayList<>();
 
     @Builder
-    public Board(Member member, String title, String content, Integer view) {
-        this.member = member;
+    public Post(String title, String content, Integer view, Member member, List<Reply> replies) {
         this.title = title;
         this.content = content;
         this.view = view;
+        this.member = member;
+        this.replies = replies;
     }
 
     public void updateBoard(String title, String content) {
