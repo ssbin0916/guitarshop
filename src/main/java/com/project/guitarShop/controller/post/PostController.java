d8048238b461e47b216eba829d366a5192d8b744
songseongbin <ssbin0916@naver.com>
2024-07-21 17:18:10 +0900

    refactor: 경로 변경

--- src/main/java/com/project/guitarShop/controller/post/PostController.java
+++ src/main/java/com/project/guitarShop/controller/post/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/board")
+@RequestMapping("/post")
 public class PostController {
 
     private final PostService postService;
