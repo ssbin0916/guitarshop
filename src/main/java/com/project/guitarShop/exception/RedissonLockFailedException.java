ff1473cddb6ac85d1c1a468ef74a954342f90fb4
songseongbin <ssbin0916@naver.com>
2024-07-22 09:33:28 +0900

    feat: redisson 예외 추가

---
+++ src/main/java/com/project/guitarShop/exception/RedissonLockFailedException.java
@@ -0,0 +1,20 @@
+package com.project.guitarShop.exception;
+
+public class RedissonLockFailedException extends RuntimeException {
+
+    public RedissonLockFailedException() {
+        super();
+    }
+
+    public RedissonLockFailedException(String message) {
+        super(message);
+    }
+
+    public RedissonLockFailedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RedissonLockFailedException(Throwable cause) {
+        super(cause);
+    }
+}
