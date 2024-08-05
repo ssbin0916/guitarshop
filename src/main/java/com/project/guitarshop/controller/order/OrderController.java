7a025432c4946fbb7957ea3b8a3e23b0ee98672f
songseongbin <ssbin0916@naver.com>
2024-08-05 16:11:38 +0900

    refactor: 경로 변수 수정

--- src/main/java/com/project/guitarshop/controller/order/OrderController.java
+++ src/main/java/com/project/guitarshop/controller/order/OrderController.java
@@ -16,19 +16,19 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/{memberId}/{itemId}/order")
-    public ResponseEntity<CreateOrderResponse> order(@Valid @PathVariable Long memberId, @Valid @PathVariable Long itemId, @Valid @RequestBody CreateOrderRequest request) throws InterruptedException {
-        CreateOrderResponse order = orderService.order(memberId, itemId, request.quantity());
-        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    @PostMapping("/order/{itemId}")
+    public ResponseEntity<CreateOrderResponse> order(@PathVariable Long itemId, @Valid @RequestBody CreateOrderRequest request) throws InterruptedException {
+        CreateOrderResponse orderResponse = orderService.order(itemId, request.quantity());
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderResponse);
     }
 
-    @PostMapping("/cancel/{id}")
-    public ResponseEntity<String> cancel(@Valid @PathVariable Long id) throws InterruptedException {
+    @PostMapping("/cancel/{orderId}")
+    public ResponseEntity<String> cancel(@PathVariable Long orderId) throws InterruptedException {
         return ResponseEntity.ok("주문이 취소되었습니다.");
     }
 
     @PostMapping("/orderFromCart/{cartId}")
-    public ResponseEntity<CreateOrderFromCartResponse> orderFromCart(@Valid @PathVariable Long cartId, @Valid @RequestBody CreateOrderFromCartRequest request) throws InterruptedException {
+    public ResponseEntity<CreateOrderFromCartResponse> orderFromCart(@PathVariable Long cartId, @Valid @RequestBody CreateOrderFromCartRequest request) throws InterruptedException {
         CreateOrderFromCartResponse order = orderService.orderFromCart(cartId, request.quantity());
         return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
